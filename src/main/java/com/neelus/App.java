80a80d818c48f8e69f74f01cd1912f2aea608607
Neelaveni0620 <kaveripakamneelaveni@gamil.com>
2023-06-21 00:16:33 +0530

    modified

---
+++ src/main/java/com/neelus/App.java
@@ -0,0 +1,13 @@
+package com.neelus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World welocme to jar!" );
+    }
+}
